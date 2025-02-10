09feceb90a67059c0e46686a814b700b7f13b8f6
jesusssy <jesmor0536@stu.mylusd.org>
2025-02-10 11:16:35 -0800

    Create temp_converter.py

---
+++ temp_converter.py
@@ -0,0 +1,18 @@
+def celsius_to_fahrenheit(temp):
+  return (temp*9/5) + 32
+print (celsius_to_fahrenheit(37.778))
+def fahrenheit_to_celsius(temp):
+  return ((temp-32) * 5/9)
+print (fahrenheit_to_celsius(100))
+def fahrenheit_to_kelvin(temp):
+  return (temp-32)* 5/9 + 273.15
+print (fahrenheit_to_kelvin(2))
+def kelvin_to_fahrenheit(temp):
+  return (temp - 273.15) * 9/5 + 32
+print (kelvin_to_fahrenheit(256.4833333333333))
+def kelvin_to_celcius(temp):
+  return temp - 273.15
+print (kelvin_to_celcius(275.15))
+def celcius_to_kelvin(temp):
+  return temp + 273.15 
+print (celcius_to_kelvin(2))
